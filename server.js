4bb81b51dcd81b5480620622ddcdfb515383b6f0
mubin86 <52404512+mubin86@users.noreply.github.com>
2020-07-29 13:34:14 +0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// Socket
+const io = require("socket.io")(http);
+
+const users = {};
+
+io.on("connection", (socket) => {
+  //console.log('Connected browser')
+  socket.on("new-user-joined", (name) => {
+    console.log("New User", name);
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  });
+
+  socket.on("message", (msg) => {
+    socket.broadcast.emit("message", msg);
+  });
+
+  //when any user left the browser
+  socket.on("disconnect", (message) => {
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
+  });
+});
